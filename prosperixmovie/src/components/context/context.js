2b8910673f8e75280702e6d73f9f19988d7400f3
akash7293 <akashkothawade77@gmail.com>
2022-12-26 15:34:11 +0530

    modal system implemented

--- prosperixmovie/src/components/context/context.js
+++ prosperixmovie/src/components/context/context.js
@@ -4,7 +4,7 @@ import React, { useContext } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const AppContext = React.createContext();
 
@@ -19,6 +19,7 @@ const AppProvider = ({ children }) => {
 	const [query, setQuery] = useState('game');
 
 	const getMovies = async (url) => {
+		setIsLoading(true);
 		try {
 			const res = await fetch(url);
 			const data = await res.json();
